d1d1f2da2ecc430405e9fdeb83e6204e091e7163
Muhammad Nauman <muhammadnauman475@gmail.com>
2020-05-07 05:30:55 +0500

    added field

--- src/components/contact/form.jsx
+++ src/components/contact/form.jsx
@@ -1,36 +1,29 @@
 import React from 'react';
 
 const Index = () => (
-  <form
-    name="contact"
-    method="post"
-    action="/success"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
-  >
+  <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
     <input type="hidden" name="bot-field" />
-    <div>
-      <label htmlFor="name">
-        Name
-        <input type="text" name="name" id="name" />
-      </label>
+    <input type="hidden" name="form-name" value="contact" />
+    <div className="field half first">
+      <label htmlFor="name">Name</label>
+      <input type="text" name="name" id="name" />
     </div>
-    <div>
-      <label htmlFor="email">
-        Email
-        <input type="text" name="email" id="email" />
-      </label>
+    <div className="field half">
+      <label htmlFor="email">Email</label>
+      <input type="text" name="email" id="email" />
     </div>
-    <div>
-      <label htmlFor="message">
-        Message
-        <textarea name="message" id="message" rows="6" required />
-      </label>
+    <div className="field">
+      <label htmlFor="message">Message</label>
+      <textarea name="message" id="message" rows="6" />
     </div>
-    <div>
-      <input type="submit" value="Drop a line" />
-      <input type="reset" value="Eraser" />
-    </div>
+    <ul className="actions">
+      <li>
+        <input type="submit" value="Send Message" className="special" />
+      </li>
+      <li>
+        <input type="reset" value="Clear" />
+      </li>
+    </ul>
   </form>
 );
 
