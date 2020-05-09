eed26390fcaf43ffe1897e54a2a73b575ba69290
Muhammad Nauman <muhammadnauman475@gmail.com>
2020-05-10 01:19:58 +0500

    menu

--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,23 +1,36 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
+import logo from '../../images/menu-logo.jpg';
+
+const Navbar = styled.div`
+.nav-item {
+  padding-right:50px !important;
+  font-family: ${(props) => props.theme.btn_font} !important;
+  color:#333 !important;
+  font-weight:700;
+  text-transform: uppercase;
+}
+`;
 
 const Header = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-white">
-    <Link className="navbar-brand" to="/">Navbar</Link>
+    <img src={logo} alt=" Logo added" />
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
-    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
+    <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+      <Navbar className="navbar-nav">
         <Link className="nav-item nav-link active" to="/">
           Home
           {' '}
           <span className="sr-only">(current)</span>
         </Link>
-        <Link className="nav-item nav-link" to="/about">Features</Link>
-        <Link className="nav-item nav-link" to="/">Contact</Link>
-        <Link className="nav-item nav-link " to="/">Disabled</Link>
-      </div>
+        <Link className="nav-item nav-link" to="/about">About</Link>
+        <Link className="nav-item nav-link" to="/services">Services</Link>
+        <Link className="nav-item nav-link " to="/">Projects</Link>
+        <Link className="nav-item nav-link " to="/contact">Contact</Link>
+      </Navbar>
     </div>
   </nav>
 );
--- src/components/contact/form.jsx
+++ src/components/contact/form.jsx
@@ -1,30 +1,125 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useFormik } from 'formik';
 
-const Index = () => (
-  <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-    <input type="hidden" name="bot-field" />
-    <input type="hidden" name="form-name" value="contact" />
-    <div className="field half first">
-      <label htmlFor="name">Name</label>
-      <input type="text" name="name" id="name" />
-    </div>
-    <div className="field half">
-      <label htmlFor="email">Email</label>
-      <input type="text" name="email" id="email" />
-    </div>
-    <div className="field">
-      <label htmlFor="message">Message</label>
-      <textarea name="message" id="message" rows="6" />
-    </div>
-    <ul className="actions">
-      <li>
-        <input type="submit" value="Send Message" className="special" />
-      </li>
-      <li>
-        <input type="reset" value="Clear" />
-      </li>
-    </ul>
-  </form>
-);
+const Wraper = styled.section`
 
+`;
+
+const SectionHeading = styled.h2`
+
+`;
+
+const Label = styled.label`
+  width: 100% !important;
+`;
+
+const Input = styled.input`
+   padding:10px !important;
+   border:2px solid #ccc !important;
+`;
+
+
+const Index = () => {
+  const { handleSubmit, handleChange } = useFormik({
+    initialValues: {
+      firstName: '',
+      lastName: '',
+    },
+    onSubmit(values) {
+      console.log(values);
+    },
+  });
+
+  return (
+
+    <Wraper>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <SectionHeading>
+              Questions? Drop us a line:
+            </SectionHeading>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <form
+              className="my-5"
+              name="contact"
+              method="post"
+            // data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              onSubmit={handleSubmit}
+            >
+              {/* <input type="hidden" name="bot-field" />
+            <input type="hidden" name="contact" value="contact" /> */}
+              <div className="form-row">
+                <div className="form-group col-md-6">
+                  <Label htmlFor="InputFirstName">
+                    First Name
+                    <Input
+                      type="text"
+                      className="form-control"
+                      id="FN"
+                      name="firstName"
+                      placeholder="First Name"
+                      onChange={handleChange}
+                    />
+                  </Label>
+                </div>
+                <div className="form-group col-md-6">
+                  <Label htmlFor="InputLastName">
+                    Last Name
+                    <Input
+                      type="text"
+                      className="form-control"
+                      id="LN"
+                      name="lastname"
+                      placeholder="Last Name"
+                      onChange={handleChange}
+                    />
+                  </Label>
+                </div>
+                <div className="form-group col-md-6">
+                  <Label htmlFor="inputEmail4">
+                    Email
+                    <Input type="email" className="form-control" id="inputEmail4" placeholder="Email" />
+                  </Label>
+                </div>
+                <div className="form-group col-md-6">
+                  <Label htmlFor="IP">
+                    Phone
+                    <Input type="phone" className="form-control" id="IP" placeholder="Phone" />
+                  </Label>
+                </div>
+              </div>
+              <div className="form-group">
+                <Label htmlFor="inputAddress">
+                  Country
+                  <Input type="text" className="form-control" id="inputAddress" placeholder="Country" />
+                </Label>
+              </div>
+              <div className="form-group">
+                <Label htmlFor="inputAddress2">
+                  City
+                  <Input type="text" className="form-control" id="inputAddress2" placeholder="City" />
+                </Label>
+              </div>
+
+              <div className="form-group">
+                <Label htmlFor="comment">
+                  Comment:
+                  <textarea className="form-control" rows="5" id="comment" />
+                </Label>
+              </div>
+
+              <button type="submit" className="btn btn-primary">Send Message</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </Wraper>
+  );
+};
 export default Index;
